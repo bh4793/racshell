29bf98ad17333e7874d632a65466dbf9034a015c
bh4793 <lmd@danskebank.dk>
2026-06-18 15:22:10 +0200

    process json for adduser command

---
+++ racshell/adduser.cpp
@@ -0,0 +1,109 @@
+#include <argparse.hpp>
+#include <string>
+#include <iostream>
+#include <vector>
+
+//#include "sear/sear.h"
+#include "lib/command_helper.hpp"
+#include <nlohmann/json.hpp>
+
+int main(int argc, char *argv[])
+{
+    argparse::ArgumentParser program("adduser");
+
+    program.add_argument("userid")
+        .help("RACF userid to create");
+
+    program.add_argument("-t", "--trait")
+        .help("trait to set, e.g. base:name='John Doe' omvs:uid=24 omvs:home_directory=/home/USER")
+        .nargs(argparse::nargs_pattern::any);
+
+    racshell::add_toggle_argument(program, "-d", "--debug", "debug sear request and response");
+
+    try
+    {
+        program.parse_args(argc, argv);
+    }
+    catch (const std::exception &err)
+    {
+        std::cerr << err.what() << std::endl;
+        std::cerr << program;
+        return 1;
+    }
+
+    std::string userid = program.get<std::string>("userid");
+
+    if (userid.length() > 8)
+    {
+        std::cerr << "\u001b[31mRACSHELL Error\x1b[0m: Invalid input, must be a valid RACF userid\n";
+        return 1;
+    }
+
+    bool debug = program.get<bool>("debug");
+
+    // Build traits object from --trait key=value pairs
+    nlohmann::json traits = nlohmann::json::object();
+    auto trait_args = program.get<std::vector<std::string>>("trait");
+    for (const auto &trait : trait_args)
+    {
+        auto sep = trait.find('=');
+        if (sep == std::string::npos)
+        {
+            std::cerr << "\u001b[31mRACSHELL Error\x1b[0m: trait must be in key=value format, got: " << trait << "\n";
+            return 1;
+        }
+        std::string key = trait.substr(0, sep);
+        std::string value = trait.substr(sep + 1);
+
+        // Try to parse as integer, otherwise keep as string
+        try {
+            size_t pos; // variable to store the number of characters processed by stoll
+            long long int_val = std::stoll(value, &pos);
+            if (pos == value.size()) {
+                traits[key] = int_val;
+                continue;
+            }
+        } catch (...) {}
+
+        traits[key] = value;
+    }
+
+    nlohmann::json req = {
+        {"operation", "add"},
+        {"admin_type", "user"},
+        {"userid", userid},
+        {"traits", traits}
+    };
+
+    std::string request_json = req.dump();
+    // sear_result_t *result = sear(request_json.c_str(), request_json.length(), debug);
+
+    // nlohmann::json response = nlohmann::json::parse(result->result_json);
+    // nlohmann::json return_codes = response.value("return_codes", nlohmann::json::object());
+    // int sear_rc = return_codes.value("sear_return_code", 0);
+    // int saf_rc  = return_codes.value("saf_return_code", 0);
+    // int racf_rc = return_codes.value("racf_return_code", 0);
+    // int racf_reason = return_codes.value("racf_reason_code", 0);
+
+    // if (sear_rc != 0 || saf_rc != 0 || racf_rc != 0)
+    // {
+    //     std::cerr << "RACSHELL Error: request failed (sear=" << sear_rc
+    //               << ", saf=" << saf_rc
+    //               << ", racf=" << racf_rc
+    //               << ", reason=" << racf_reason << ")\n";
+
+    //     // Print any SEAR errors from the response
+    //     if (response.contains("errors") && response["errors"].is_array())
+    //     {
+    //         for (const auto &err : response["errors"])
+    //         {
+    //             if (err.is_string())
+    //                 std::cerr << "  " << err.get<std::string>() << "\n";
+    //         }
+    //     }
+    //     return 1;
+    // }
+
+    std::cout << "User " << request_json << " created successfully.\n";
+    return 0;
+}
