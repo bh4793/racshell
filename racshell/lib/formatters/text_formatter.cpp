a3b4a71a9acd88b4088b7cc14d3dbecd2f6a3e94
bh4793 <lmd@danskebank.dk>
2026-06-18 10:23:01 +0200

    add format to omvs and tso

--- racshell/lib/formatters/text_formatter.cpp
+++ racshell/lib/formatters/text_formatter.cpp
@@ -21,11 +21,31 @@ std::string TextFormatter::format(const UserData& user) {
     }
     
     if (!user.tso.is_null() && !user.tso.empty()) {
-        ss << "TSO: " << user.tso.dump() << "\n";
+        ss << "TSO:\n";
+        if (user.tso.contains("tso:logon_procedure"))
+            ss << "  Procedure:    " << user.tso["tso:logon_procedure"].get<std::string>() << "\n";
+        if (user.tso.contains("tso:account_number"))
+            ss << "  Account:      " << user.tso["tso:account_number"].get<std::string>() << "\n";
+        if (user.tso.contains("tso:default_region_size"))
+            ss << "  Region:       " << user.tso["tso:default_region_size"] << "\n";
+        if (user.tso.contains("tso:max_region_size"))
+            ss << "  Max region:   " << user.tso["tso:max_region_size"] << "\n";
+        if (user.tso.contains("tso:dataset_allocation_unit"))
+            ss << "  Alloc unit:   " << user.tso["tso:dataset_allocation_unit"].get<std::string>() << "\n";
+        if (user.tso.contains("tso:logon_command"))
+            ss << "  Logon cmd:    " << user.tso["tso:logon_command"].get<std::string>() << "\n";
+        if (user.tso.contains("tso:user_data"))
+            ss << "  User data:    " << user.tso["tso:user_data"].get<std::string>() << "\n";
     }
     
     if (!user.omvs.is_null() && !user.omvs.empty()) {
-        ss << "OMVS: " << user.omvs.dump() << "\n";
+        ss << "OMVS:\n";
+        if (user.omvs.contains("omvs:uid"))
+            ss << "  UID:   " << user.omvs["omvs:uid"] << "\n";
+        if (user.omvs.contains("omvs:home_directory"))
+            ss << "  Home:  " << user.omvs["omvs:home_directory"].get<std::string>() << "\n";
+        if (user.omvs.contains("omvs:default_shell"))
+            ss << "  Shell: " << user.omvs["omvs:default_shell"].get<std::string>() << "\n";
     }
     
     if (!user.kerberos.is_null() && !user.kerberos.empty()) {
