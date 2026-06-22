5a97a5f8527902b3b7701fa87e133c333851eda4
bh4793 <lmd@danskebank.dk>
2026-06-22 13:06:03 +0200

    refactor: add entity, adduser, addgroup

--- racshell/addgroup.cpp
+++ racshell/addgroup.cpp
@@ -1,81 +1,16 @@
-#include <argparse.hpp>
-#include <string>
-#include <iostream>
-#include <vector>
-
-#include "sear/sear.h"
-#include "lib/command_helper.hpp"
-#include <nlohmann/json.hpp>
+#include "lib/add_entity_command.hpp"
 
 int main(int argc, char *argv[])
 {
-    argparse::ArgumentParser program("addgroup");
-
-    program.add_argument("group")
-        .help("RACF group to create");
-
-    program.add_argument("-t", "--traits")
-        .help("traits to set, e.g. base:owner=SYS1 base:superior_group=SYS1")
-        .nargs(argparse::nargs_pattern::any);
-
-    racshell::add_no_color_argument(program);
-    racshell::add_toggle_argument(program, "-d", "--debug", "debug sear request and response");
-    racshell::add_toggle_argument(program, "-a", "--all-json", "output full raw SEAR JSON response");
-
-    try
-    {
-        program.parse_args(argc, argv);
-    }
-    catch (const std::exception &err)
-    {
-        racshell::print_error(std::cerr, err.what());
-        std::cerr << program;
-        return 1;
-    }
-
-    racshell::set_color_output_enabled(!program.get<bool>("no-color"));
-
-    std::string group = program.get<std::string>("group");
-    if (group.length() > 8)
-    {
-        racshell::print_error(std::cerr, "Invalid input, must be a valid RACF group name");
-        return 1;
-    }
-
-    bool debug = program.get<bool>("debug");
-    bool all_json = program.get<bool>("all-json");
-
-    nlohmann::json traits;
-    if (!racshell::parse_traits(program.get<std::vector<std::string>>("traits"), traits))
-    {
-        return 1;
-    }
-
-    nlohmann::json req = {
-        {"operation", "add"},
-        {"admin_type", "group"},
-        {"group", group},
-        {"traits", traits}
+    const racshell::AddEntityCommandSpec spec = {
+        "addgroup",
+        "group",
+        "group",
+        "RACF group to create",
+        "Invalid input, must be a valid RACF group name",
+        "traits to set, e.g. base:owner=SYS1 base:superior_group=SYS1",
+        "Group"
     };
 
-    std::string request_json = req.dump();
-    sear_result_t *result = sear(request_json.c_str(), request_json.length(), debug);
-
-    if (all_json)
-    {
-        std::cout << result->result_json << "\n";
-        return 0;
-    }
-
-    racshell::SearOperationInfo op_info = racshell::validate_sear_operation_result(result->result_json);
-    if (!op_info.success)
-    {
-        racshell::print_error(std::cerr, op_info.error_message);
-        racshell::print_sear_errors(op_info.response, std::cerr);
-        return 1;
-    }
-
-    racshell::print_success_prefix(std::cout);
-    std::cout << "Group " << group << " created successfully.\n";
-    return 0;
+    return racshell::run_add_entity_command(argc, argv, spec);
 }
---
+++ racshell/lib/add_entity_command.hpp
@@ -0,0 +1,90 @@
+#pragma once
+
+#include <argparse.hpp>
+#include <nlohmann/json.hpp>
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "sear/sear.h"
+#include "lib/command_helper.hpp"
+
+namespace racshell {
+
+struct AddEntityCommandSpec {
+    const char* command_name;
+    const char* admin_type;
+    const char* entity_argument;
+    const char* entity_help;
+    const char* entity_validation_error;
+    const char* traits_help;
+    const char* success_label;
+};
+
+inline int run_add_entity_command(int argc, char* argv[], const AddEntityCommandSpec& spec) {
+    argparse::ArgumentParser program(spec.command_name);
+
+    program.add_argument(spec.entity_argument)
+        .help(spec.entity_help);
+
+    program.add_argument("-t", "--traits")
+        .help(spec.traits_help)
+        .nargs(argparse::nargs_pattern::any);
+
+    add_no_color_argument(program);
+    add_toggle_argument(program, "-d", "--debug", "debug sear request and response");
+    add_toggle_argument(program, "-a", "--all-json", "output full raw SEAR JSON response");
+
+    try {
+        program.parse_args(argc, argv);
+    } catch (const std::exception& err) {
+        print_error(std::cerr, err.what());
+        std::cerr << program;
+        return 1;
+    }
+
+    set_color_output_enabled(!program.get<bool>("no-color"));
+
+    std::string entity_value = program.get<std::string>(spec.entity_argument);
+    if (entity_value.length() > 8) {
+        print_error(std::cerr, spec.entity_validation_error);
+        return 1;
+    }
+
+    const bool debug = program.get<bool>("debug");
+    const bool all_json = program.get<bool>("all-json");
+
+    nlohmann::json traits;
+    if (!parse_traits(program.get<std::vector<std::string>>("traits"), traits)) {
+        return 1;
+    }
+
+    nlohmann::json request = {
+        {"operation", "add"},
+        {"admin_type", spec.admin_type},
+        {spec.entity_argument, entity_value},
+        {"traits", traits}
+    };
+
+    const std::string request_json = request.dump();
+    sear_result_t* result = sear(request_json.c_str(), request_json.length(), debug);
+
+    if (all_json) {
+        std::cout << result->result_json << "\n";
+        return 0;
+    }
+
+    SearOperationInfo op_info = validate_sear_operation_result(result->result_json);
+    if (!op_info.success) {
+        print_error(std::cerr, op_info.error_message);
+        print_sear_errors(op_info.response, std::cerr);
+        return 1;
+    }
+
+    print_success_prefix(std::cout);
+    std::cout << spec.success_label << " " << entity_value << " created successfully.\n";
+    return 0;
+}
+
+}  // namespace racshell
