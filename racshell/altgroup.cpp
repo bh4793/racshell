c4db11116c4a6ef396e55c8a2c83321ee1296998
bh4793 <lmd@danskebank.dk>
2026-06-23 08:31:39 +0200

    format

--- racshell/altgroup.cpp
+++ racshell/altgroup.cpp
@@ -1,14 +1,13 @@
 #include "lib/alter_entity_command.hpp"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     const racshell::AlterEntityCommandSpec spec = {
         "altgroup", "group", "group",
         "RACF group to alter",
         "Invalid input, must be a valid RACF group",
         "traits to alter, e.g. base:owner='SYSPROG' omvs:gid=1234567",
-        "Group"
-    };
+        "Group"};
 
     return racshell::run_alter_entity_command(argc, argv, spec);
 }
--- racshell/altuser.cpp
+++ racshell/altuser.cpp
@@ -1,14 +1,13 @@
 #include "lib/alter_entity_command.hpp"
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     const racshell::AlterEntityCommandSpec spec = {
         "altuser", "user", "userid",
         "RACF userid to alter",
         "Invalid input, must be a valid RACF userid",
         "traits to alter, e.g. base:name='LEONARD CARCARAMO' base:special=true",
-        "User"
-    };
+        "User"};
 
     return racshell::run_alter_entity_command(argc, argv, spec);
 }
