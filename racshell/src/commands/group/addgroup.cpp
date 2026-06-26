ff03fc410189fd36861ca3a32270dd4dcb711a3a
bh4793 <lmd@danskebank.dk>
2026-06-26 09:42:42 +0200

    refactor: traits in addgroup

--- racshell/src/commands/group/addgroup.cpp
+++ racshell/src/commands/group/addgroup.cpp
@@ -1,7 +1,26 @@
 #include "include/racshell/commands/add_entity_command.hpp"
 
+#include <string>
+#include <vector>
+
+struct TraitArg
+{
+    std::string short_flag;
+    std::string long_flag;
+    std::string trait_key;
+    std::string help;
+    bool is_flag = false;
+};
+
 int main(int argc, char *argv[])
 {
+    const std::vector<TraitArg> trait_args = {
+        {"-s", "--superior-group", "base:superior_group", "superior group for the new group"},
+        {"-o", "--owner",          "base:owner",          "owner of the new group"},
+        {"-g", "--omvs-gid",       "omvs:gid",            "z/OS Unix group ID (GID) for the new group"},
+        {"",   "--omvs-auto-gid",  "omvs:auto_gid",       "automatically assign a z/OS Unix GID to the new group", true},
+    };
+
     const racshell::AddEntityCommandSpec spec = {
         .command_name = "addgroup",
         .admin_type = "group",
@@ -10,35 +29,31 @@ int main(int argc, char *argv[])
         .entity_validation_error = "Invalid input, must be a valid RACF group name",
         .traits_help = "traits to set, e.g. base:owner=SYS1 base:superior_group=SYS1",
         .success_label = "Group",
-        .setup_extra_args = [](argparse::ArgumentParser &program)
+        .setup_extra_args = [trait_args](argparse::ArgumentParser &program)
         {
-            program.add_argument("-s", "--superior-group")
-                .help("superior group for the new group");
-            program.add_argument("-o", "--owner")
-                .help("owner of the new group");
-            program.add_argument("-g", "--omvs-gid")
-                .help("z/OS Unix group ID (GID) for the new group");
-            program.add_argument("--omvs-auto-gid")
-                .help("automatically assign a z/OS Unix GID to the new group")
-                .flag();
+            for (const auto &t : trait_args)
+            {
+                auto &arg = t.short_flag.empty()
+                    ? program.add_argument(t.long_flag)
+                    : program.add_argument(t.short_flag, t.long_flag);
+                arg.help(t.help);
+                if (t.is_flag)
+                    arg.flag();
+            }
         },
-        .apply_extra_args = [](argparse::ArgumentParser &program, nlohmann::json &request)
+        .apply_extra_args = [trait_args](argparse::ArgumentParser &program, nlohmann::json &request)
         {
-            if (auto v = program.present("--superior-group"))
+            for (const auto &t : trait_args)
             {
-                request["traits"]["base:superior_group"] = *v;
-            }
-            if (auto v = program.present("--owner"))
-            {
-                request["traits"]["base:owner"] = *v;
-            }
-            if (auto v = program.present("--omvs-gid"))
-            {
-                request["traits"]["omvs:gid"] = *v;
-            }
-            if (program.get<bool>("--omvs-auto-gid"))
-            {
-                request["traits"]["omvs:auto_gid"] = true;
+                if (t.is_flag)
+                {
+                    if (program.get<bool>(t.long_flag))
+                        request["traits"][t.trait_key] = true;
+                }
+                else if (auto v = program.present<std::string>(t.long_flag))
+                {
+                    request["traits"][t.trait_key] = *v;
+                }
             }
         }};
 
