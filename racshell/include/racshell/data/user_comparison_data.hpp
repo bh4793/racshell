a7538f3526401d51254cdf6360b16b9b5fdb49a6
bh4793 <lmd@danskebank.dk>
2026-07-01 09:39:44 +0200

    add flags to the user comparison

--- racshell/include/racshell/data/user_comparison_data.hpp
+++ racshell/include/racshell/data/user_comparison_data.hpp
@@ -9,12 +9,21 @@ struct UserComparisonSide
     std::string userid;
     std::string raw_response_json;
     nlohmann::json response_json;
+    nlohmann::json profile;
+    nlohmann::json base;
 };
 
 struct UserComparisonData
 {
     UserComparisonSide left;
     UserComparisonSide right;
+    bool compare_groups = false;
+    bool compare_security = false;
+    bool compare_tso = false;
+    bool compare_kerberos = false;
+    bool compare_cics = false;
+    bool compare_omvs = false;
+    bool compare_csdata = false;
     bool raw_json_output = false;
     bool identical = false;
     nlohmann::json differences;
--- racshell/src/commands/user/compareusers.cpp
+++ racshell/src/commands/user/compareusers.cpp
@@ -80,12 +80,26 @@ namespace
 
     UserComparisonData build_user_comparison_data(const std::string &left_userid,
                                                   const std::string &right_userid,
+                                                  bool compare_groups,
+                                                  bool compare_security,
+                                                  bool compare_tso,
+                                                  bool compare_kerberos,
+                                                  bool compare_cics,
+                                                  bool compare_omvs,
+                                                  bool compare_csdata,
                                                   bool debug,
                                                   bool raw_json_output)
     {
         UserComparisonData comparison;
         comparison.left.userid = left_userid;
         comparison.right.userid = right_userid;
+        comparison.compare_groups = compare_groups;
+        comparison.compare_security = compare_security;
+        comparison.compare_tso = compare_tso;
+        comparison.compare_kerberos = compare_kerberos;
+        comparison.compare_cics = compare_cics;
+        comparison.compare_omvs = compare_omvs;
+        comparison.compare_csdata = compare_csdata;
         comparison.raw_json_output = raw_json_output;
 
         comparison.left.raw_response_json = racshell::execute_extract_request("user", "userid", left_userid, debug);
@@ -134,6 +148,11 @@ namespace
             throw std::runtime_error(right_userid + ": " + error_message);
         }
 
+        comparison.left.profile = left_profile;
+        comparison.left.base = left_base;
+        comparison.right.profile = right_profile;
+        comparison.right.base = right_base;
+
         racshell::add_difference(comparison.differences, "name",
                                  racshell::get_object_value(left_base, "base:name"),
                                  racshell::get_object_value(right_base, "base:name"));
@@ -150,29 +169,50 @@ namespace
                                  racshell::get_object_value(left_base, "base:revoked"),
                                  racshell::get_object_value(right_base, "base:revoked"));
 
-        racshell::add_list_difference(comparison.differences,
-                                      "groups",
-                                      extract_groups(left_base),
-                                      extract_groups(right_base));
+        if (compare_security)
+        {
+            racshell::add_difference(comparison.differences, "security",
+                                     build_security_snapshot(left_base, left_profile),
+                                     build_security_snapshot(right_base, right_profile));
+        }
+        if (compare_tso)
+        {
+            racshell::add_difference(comparison.differences, "tso",
+                                     racshell::get_object_value(left_profile, "tso"),
+                                     racshell::get_object_value(right_profile, "tso"));
+        }
+        if (compare_kerberos)
+        {
+            racshell::add_difference(comparison.differences, "kerberos",
+                                     racshell::get_object_value(left_profile, "kerberos"),
+                                     racshell::get_object_value(right_profile, "kerberos"));
+        }
+        if (compare_cics)
+        {
+            racshell::add_difference(comparison.differences, "cics",
+                                     racshell::get_object_value(left_profile, "cics"),
+                                     racshell::get_object_value(right_profile, "cics"));
+        }
 
-        racshell::add_difference(comparison.differences, "security",
-                                 build_security_snapshot(left_base, left_profile),
-                                 build_security_snapshot(right_base, right_profile));
-        racshell::add_difference(comparison.differences, "tso",
-                                 racshell::get_object_value(left_profile, "tso"),
-                                 racshell::get_object_value(right_profile, "tso"));
-        racshell::add_difference(comparison.differences, "omvs",
-                                 racshell::get_object_value(left_profile, "omvs"),
-                                 racshell::get_object_value(right_profile, "omvs"));
-        racshell::add_difference(comparison.differences, "kerberos",
-                                 racshell::get_object_value(left_profile, "kerberos"),
-                                 racshell::get_object_value(right_profile, "kerberos"));
-        racshell::add_difference(comparison.differences, "cics",
-                                 racshell::get_object_value(left_profile, "cics"),
-                                 racshell::get_object_value(right_profile, "cics"));
-        racshell::add_difference(comparison.differences, "csdata",
-                                 racshell::get_object_value(left_profile, "csdata"),
-                                 racshell::get_object_value(right_profile, "csdata"));
+        if (compare_groups)
+        {
+            racshell::add_list_difference(comparison.differences,
+                                          "groups",
+                                          extract_groups(left_base),
+                                          extract_groups(right_base));
+        }
+        if (compare_omvs)
+        {
+            racshell::add_difference(comparison.differences, "omvs",
+                                     racshell::get_object_value(left_profile, "omvs"),
+                                     racshell::get_object_value(right_profile, "omvs"));
+        }
+        if (compare_csdata)
+        {
+            racshell::add_difference(comparison.differences, "csdata",
+                                     racshell::get_object_value(left_profile, "csdata"),
+                                     racshell::get_object_value(right_profile, "csdata"));
+        }
 
         comparison.identical = comparison.differences.empty();
         return comparison;
@@ -190,6 +230,13 @@ int main(int argc, char *argv[])
     program.add_argument("right-user")
         .help("second RACF user to compare");
 
+    racshell::add_toggle_argument(program, "-u", "--groups", "compare groups");
+    racshell::add_toggle_argument(program, "-s", "--security", "compare security data");
+    racshell::add_toggle_argument(program, "-t", "--tso", "compare TSO segment");
+    racshell::add_toggle_argument(program, "-k", "--kerberos", "compare Kerberos segment");
+    racshell::add_toggle_argument(program, "-c", "--cics", "compare CICS segment");
+    racshell::add_toggle_argument(program, "-o", "--omvs", "compare OMVS segment");
+    racshell::add_toggle_argument(program, "-x", "--csdata", "compare CSDATA segment");
     racshell::add_no_color_argument(program);
     racshell::add_toggle_argument(program, "-d", "--debug", "debug sear request and response");
     racshell::add_toggle_argument(program, "-j", "--json", "output as JSON");
@@ -216,6 +263,13 @@ int main(int argc, char *argv[])
         return 1;
     }
 
+    const bool compare_groups = program.get<bool>("groups");
+    const bool compare_security = program.get<bool>("security");
+    const bool compare_tso = program.get<bool>("tso");
+    const bool compare_kerberos = program.get<bool>("kerberos");
+    const bool compare_cics = program.get<bool>("cics");
+    const bool compare_omvs = program.get<bool>("omvs");
+    const bool compare_csdata = program.get<bool>("csdata");
     const bool debug = program.get<bool>("debug");
     const bool json_output = program.get<bool>("json");
     const bool all_json = program.get<bool>("all-json");
@@ -224,6 +278,13 @@ int main(int argc, char *argv[])
     {
         const UserComparisonData comparison = build_user_comparison_data(left_userid,
                                                                          right_userid,
+                                                                         compare_groups,
+                                                                         compare_security,
+                                                                         compare_tso,
+                                                                         compare_kerberos,
+                                                                         compare_cics,
+                                                                         compare_omvs,
+                                                                         compare_csdata,
                                                                          debug,
                                                                          all_json);
 
